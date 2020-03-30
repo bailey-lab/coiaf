5f23881557aad8e68ddf55e2cf8a2388fd7be13e
arisp99 <aris.paschalidis@gmail.com>
2020-03-30 19:09:05 -0400

    Creation of new function to calculate theoretical COI curve, given a COI and an interval over which COI will be calculated.

---
+++ R/theoretical_coi.R
@@ -0,0 +1,15 @@
+#------------------------------------------------
+#' @title Generate Theoretical COI Curve
+#'
+#' @description Generate theoretical COI curve. At each locus defined as:
+#' \eqn{1-p^coi-q^coi}. This expression is derived in the corresponding
+#' manuscript.
+#'
+#' @param coi The COI
+#' @param interval The interval over which the curve will be calculated
+#' @return The theoretical COI curve for the specified interval.
+
+theoretical_coi <- function(coi, interval){
+  curve <- 1 - interval^coi - (1 - interval)^coi
+  return(curve)
+}
