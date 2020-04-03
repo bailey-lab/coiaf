3b4c8af1264c415d5e627b6bcd70124cb157fd75
arisp99 <aris.paschalidis@gmail.com>
2020-04-03 17:48:05 -0400

    Now that computing the theoretical coi has been split into two functions, updated analysis script to account for that change. Also updated test file.

--- tests/testthat/test_theoretical_coi.R
+++ tests/testthat/test_theoretical_coi.R
@@ -1,11 +1,11 @@
 context("theoretical coi")
 
 #------------------------------------------------
-test_that("theoretical_coi works", {
+test_that("single_theoretical_coi works", {
   # define coi and interval
   coi = 2
   interval = seq(0, 0.5, 0.25)
 
-  curve <- theoretical_coi(coi, interval)
+  curve <- single_theoretical_coi(coi, interval)
   expect_equal(curve, c(0.00, 0.375, 0.5))
 })
