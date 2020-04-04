9c654cab35857cf11ffc5a8fe7fe9454f6816b18
arisp99 <aris.paschalidis@gmail.com>
2020-04-03 20:50:00 -0400

    Added new function called compute_coi. Takes in theoretical coi curves and simulated results and computes coi. Have done two methods so far: look at PLAF of 0.5, and look at ideal PLAF (which is calculated).

--- R/compute_coi.R
+++ R/compute_coi.R
@@ -1,7 +1,7 @@
 #------------------------------------------------
-#' @title Compute COI
+#' @title Generate Simulated COI Curve
 #'
-#' @description Compute the COI of the sample. In order to do this, we utilize
+#' @description Generate the simulated COI curve. In order to do this, we utilize
 #'  the output of \code{\link{sim_biallelic}}, which created simulated data.
 #'  We keep the PLAF, and compute whether a SNP is a variant or not, based
 #'  on the simulated WSAF at that SNP -- acounting for potential sequencing
@@ -9,15 +9,15 @@
 #'  site or not, we determine whether a site should be varaint or not using
 #'  the phased haplotype of the parasites.
 #'
-#' @param sim Output of \code{\link{sim_biallelic}}.
-#' @param seq_error The level of sequencing error that is assumed.
-#' @param cuts How often the data is summarized.
-#' @param theoretical_cois The theoretical COI curves to be examined.
-#' @return Dataframe with averaged variant for several PLAF cuts.
+#' @param sim Output of \code{\link{sim_biallelic}}
+#' @param seq_error The level of sequencing error that is assumed
+#' @param cuts How often the data is summarized
+#' @param theoretical_cois The theoretical COI curves to be examined
+#' @return Simulated COI curve
 #'
 #' @export
 
-compute_coi <- function(sim, seq_error, cuts, theoretical_cois){
+simulated_coi <- function(sim, seq_error, cuts, theoretical_cois){
   # Check inputs
   assert_single_bounded(seq_error)
 
@@ -43,4 +43,69 @@ compute_coi <- function(sim, seq_error, cuts, theoretical_cois){
   df_sim_grouped$midpoints <- cuts[-length(cuts)] + diff(cuts)/2
 
   return(df_sim_grouped)
+}
+
+#------------------------------------------------
+#' @title Compute COI
+#'
+#' @description Generate the COI of the sample by comparing the simualated
+#' coi curve to the theoretical coi curve.
+#'
+#' @param theory_cois The theoretical COI curves to be examined
+#' @param sim_coi The simulated coi curve
+#' @param method The method to be employed.
+#' @param cuts How often the data is summarized
+#' @return COI for the simulation
+#'
+#' @export
+
+compute_coi <- function(theory_cois, sim_coi, method = "end", cuts){
+  # In order to calculate the best points to find COI, need
+  # to have COI of one as well. Do COI[i] - COI[i-1].
+  # So for method "end",  need to remove the first column
+
+  # Minus 1 because theory_cois now includes the PLAF at the end
+  bound_coi = ncol(theory_cois) - 1
+  if (method == "end"){
+    # Method 1: Compare last value
+    last_theory <- theory_cois[, 2:bound_coi]
+    last_theory <- last_theory[nrow(last_theory),]
+    last_sim <- sim_coi$m_variant[nrow(sim_coi)]
+
+    coi <- stringr::str_sub(names(which.min(abs(last_theory - last_sim))), -1)
+  } else if (method == "ideal"){
+    # Method 2: Compute ideal PLAF
+    dist <- list()
+    for (i in 2:bound_coi){
+      # COI[i] - COI[i-1]
+      diff = theory_cois[i] - theory_cois[i - 1]
+
+      # Determine best PLAF value
+      ideal_PLAF <- theory_cois$PLAF[which.max(diff[[1]])]
+
+      theory_WSAF <- theory_cois[which.max(diff[[1]]), i]
+      m_var <- sim_coi$m_variant[cut(ideal_PLAF, cuts)]
+
+      dist[i-1] <- abs(theory_WSAF - m_var)
+    }
+    names(dist) <- colnames(theory_cois)[2:bound_coi]
+
+      # df_grouped$PLAF_cut[cut(0.5, cut)]
+
+      # # Determine what the WSAF value is at the best PLAF is
+      # ideal[i-1] <- theory_cois[which.max(diff[[1]]), i]
+      #
+      # # For each ideal COI curve, find out which point from simulated
+      # # curve is closet to that curve
+      # dist_to_ideal[i-1] <- min(abs(df_grouped$m_variant - ideal[[i-1]]))
+    # }
+    # Now have the WSAFs in ideal place for each COI curve
+    # names(ideal) <- colnames(theory_cois)[2:bound_coi]
+    # names(dist_to_ideal) <- names(ideal)
+
+    coi <- stringr::str_sub(names(which.min(dist)), -1)
+  }
+
+  ret_str <- paste("COI is", coi, sep = " ")
+  return(ret_str)
 }
