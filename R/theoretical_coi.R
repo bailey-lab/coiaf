7d21f2499797ce2842b67e4626bbeee7ad31adca
arisp99 <aris.paschalidis@gmail.com>
2020-04-28 22:08:07 -0400

    Added new functionality to functions used for calculating the theoretical COI. Now, user can input method 1 or 2 in order to calculate the two types of theoretical COI curves indicated in the 02 analysis file.

--- R/theoretical_coi.R
+++ R/theoretical_coi.R
@@ -4,6 +4,8 @@
 #' @description Generate theoretical COI curves. At each locus defined as:
 #' \eqn{1-p^coi-q^coi}. This expression is derived in the corresponding
 #' manuscript.
+#' @param method The method we will use to calculate the theoretical COI.
+#' The method is either "1" or "2". The default value is "1".
 #'
 #' @param cois The COIs for which the curve will be calculated.
 #' @param interval The interval over which the curve will be calculated.
@@ -12,20 +14,23 @@
 #'
 #' @export
 
-theoretical_coi <- function(cois, interval){
+theoretical_coi <- function(cois, interval, method = "1"){
   # Check inputs
   assert_pos_int(cois, zero_allowed = FALSE)
   assert_vector(interval)
   assert_bounded(interval, left = 0, right = 0.5)
   assert_increasing(interval)
+  assert_single_string(method)
+  assert_in(method, c("1", "2"))
 
   # Compute curve for the COIs
   for (i in cois){
     if (i == cois[1]){
-      curves <- data.frame(first = single_theoretical_coi(i, interval))
+      curves <- data.frame(first = single_theoretical_coi(i, interval, method))
       colnames(curves) <- paste("coi_", i, sep="")
     } else {
-      curves[[paste("coi_", i, sep="")]] = single_theoretical_coi(i, interval)
+      curves[[paste("coi_", i, sep="")]] =
+        single_theoretical_coi(i, interval, method)
     }
   }
 
@@ -42,19 +47,27 @@ theoretical_coi <- function(cois, interval){
 #'
 #' @param coi The COI.
 #' @param interval The interval over which the curve will be calculated.
+#' @param method The method we will use to calculate the theoretical COI.
+#' The method is either "1" or "2". The default value is "1".
 #'
 #' @return The theoretical COI curve for the specified interval.
 #'
 #' @keywords internal
 
-single_theoretical_coi <- function(coi, interval){
+single_theoretical_coi <- function(coi, interval, method = "1"){
   # Check inputs
   assert_single_pos_int(coi)
   assert_vector(interval)
   assert_bounded(interval, left = 0, right = 0.5)
   assert_increasing(interval)
+  assert_single_string(method)
+  assert_in(method, c("1", "2"))
 
   # Determine the curve
-  curve <- 1 - interval^coi - (1 - interval)^coi
+  if (method == "1"){
+    curve <- 1 - interval^coi - (1 - interval)^coi
+  } else if (method == "2"){
+    curve <- (interval - interval^coi)/(1 - interval^coi - (1 - interval)^coi)
+  }
   return(curve)
 }
