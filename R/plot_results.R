646327e2dab20c740648cc5f5d9e2b9af25d18cf
arisp99 <aris.paschalidis@gmail.com>
2020-06-01 12:12:06 -0400

    Added a function to print the mean absolute error and the confidence interval.

--- R/plot_results.R
+++ R/plot_results.R
@@ -44,8 +44,8 @@ sensitivity_plot <- function(data,
   assert_length(plot_dims, 2)
   if (!is.null(change_param)) {assert_string(change_param)}
   if (!is.null(change_param_val)) {assert_vector(change_param_val)}
-  if (!is.null(title)) {assert_string(title)}
-  if (!is.null(caption)) {assert_string(caption)}
+  if (!is.null(title)) {assert_single_string(title)}
+  if (!is.null(caption)) {assert_single_string(caption)}
 
   # Convert the predicted_coi dataframe into a long format. More specifically,
   # establish a column for the true coi and a column for the estimated coi
@@ -141,13 +141,97 @@ sensitivity_plot_element <- function(data,
     ggplot2::scale_size_area() +
     ggplot2::geom_abline(color = "red", size = 1) +
     ggplot2::theme_classic() +
-    ggplot2::theme(plot.title  = ggplot2::element_text(hjust = 0.5, size = 10),
-                   axis.title  = ggplot2::element_text(size = 7),
-                  legend.title = ggplot2::element_text(size = 7),
-                   legend.text = ggplot2::element_text(size = 7)) +
+    ggplot2::theme(plot.title   = ggplot2::element_text(hjust = 0.5, size = 10),
+                   axis.title   = ggplot2::element_text(size = 7),
+                   legend.title = ggplot2::element_text(size = 7),
+                   legend.text  = ggplot2::element_text(size = 7)) +
     ggplot2::labs(x = "True COI",
                   y = "Estimated COI",
                   title = paste0(change_param, change_param_val[loop_num]))
 
   return(single_plot)
+}
+
+#------------------------------------------------
+#' @title Plot the mean absolute errors for a test
+#'
+#' @description Plots the mean absolute error and the confidence interval.
+#'
+#' @param data The data to be plotted.
+#' @param fill The variable the data will be seperated by.
+#' @param fill_levels The levels for the fill variable.
+#' @param title The title of the plot. Default to \code{NULL}.
+#' @param legend_title The text for the legend. Default to \code{NULL}.
+#' @param legend.position The position of the legend. One of \code{"none",
+#' "left", "right", "bottom", "top"}.
+#'
+#' @return The plot
+#'
+#' @export
+
+error_plot <- function(data, fill = "COI", fill_levels = NULL, title = NULL,
+                       legend_title = fill, legend.position = "bottom"){
+
+  # Ensure that ggplot2 is installed
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop('Package \"ggplot2\" must be installed in order to plot the test.',
+         call. = FALSE)
+  }
+
+  # Check inputs
+  assert_single_string(fill)
+  if (!is.null(fill_levels)) {assert_string(fill_levels)}
+  if (!is.null(title)) {assert_single_string(title)}
+  assert_single_string(legend_title)
+  assert_single_string(legend.position)
+
+  # Convert data to a tibble
+  plot_data <- data$boot_error %>% tidyr::unnest(cols = names(data$boot_error))
+
+  # Make several changes to the data frame. For columns that have more than 1
+  # unique value and are not one of c("mae", "lower", "upper", "bias"), convert
+  # them to a factor. We also replace 0 with NA for the columns mae, lower, and
+  # upper. This makes plotting easier and ensures that error bars are not shown
+  # for data points where the mean absolute error is 0.
+  plot_data <- plot_data %>%
+    dplyr::mutate(dplyr::across(where(function(x) dplyr::n_distinct(x) > 1) &
+                                  !all_of(c("mae", "lower", "upper", "bias")),
+                                as.factor)) %>%
+    dplyr::mutate(mae   = dplyr::na_if(mae, 0)) %>%
+    dplyr::mutate(lower = dplyr::na_if(lower, 0)) %>%
+    dplyr::mutate(upper = dplyr::na_if(upper, 0))
+
+  # Customize the levels of the fill variable
+  if (!is.null(fill_levels)){
+    # Ensure that the number of levels input (fill_levels) are the same as the
+    # number of levels for the fill variable
+    if (length(fill_levels) != nlevels(plot_data[[fill]])){
+      stop(strwrap(sprintf("The length of the fill_levels variable (%s) does not
+      match with the number of levels of the fill variable (%s)",
+                           length(fill_levels), nlevels(plot_data[[fill]]))),
+           call. = FALSE)
+    }
+    else {
+      # Customize labels
+      levels(plot_data[[fill]]) <- fill_levels
+    }
+  }
+
+  # Plot the data and return
+  error_plot <- ggplot2::ggplot(plot_data,
+                                ggplot2::aes(x = COI, y = mae,
+                                             fill = eval(parse(text = fill)))) +
+    ggplot2::geom_col(position = position_dodge(), na.rm = T) +
+    ggplot2::geom_errorbar(ggplot2::aes(ymin = lower, ymax = upper),
+                           width = .2, position = position_dodge(.9)) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(legend.position = legend.position,
+                   plot.title   = ggplot2::element_text(hjust = 0.5, size = 10),
+                   axis.title   = ggplot2::element_text(size = 7),
+                   legend.title = ggplot2::element_text(size = 7),
+                   legend.text  = ggplot2::element_text(size = 7)) +
+    ggplot2::labs(y = "Mean Absolute Error", title = title, fill = legend_title)
+
+  return(error_plot)
+
 }
