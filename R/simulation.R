50acb04cff587edb4d3230e2e1fa6d6fe63df60e
arisp99 <aris.paschalidis@gmail.com>
2020-06-08 18:46:32 -0400

    Move the location a function to another file. The function for collecting the simulated data has now been moved to the simulation file. This was done so that similar functions are in the same file (both functions for simulating data are now in the same file).

--- R/simulation.R
+++ R/simulation.R
@@ -102,3 +102,57 @@ sim_biallelic <- function(COI = 3,
                                 WSAF = counts/coverage))
   return(ret)
 }
+
+#------------------------------------------------
+#' @title Generate Simulated COI Curve
+#'
+#' @description Generate the simulated COI curve. In order to do this, we
+#'  utilize the output of \code{\link{sim_biallelic}}, which created simulated
+#'  data. We keep the PLAF, and compute whether a SNP is a variant or not, based
+#'  on the simulated WSAF at that SNP -- accounting for potential sequencing
+#'  error. To check whether our simulated WSAF correctly indicated a variant
+#'  site or not, we determine whether a site should be variant or not using
+#'  the phased haplotype of the parasites.
+#'
+#' @param sim Output of \code{\link{sim_biallelic}}
+#' @param seq_error The level of sequencing error that is assumed.
+#' @param cuts How often the data is summarized.
+#'
+#' @return Simulated COI curve.
+#'
+#' @export
+
+simulated_coi <- function(sim, seq_error, cuts){
+  # Check inputs
+  assert_single_bounded(seq_error)
+  assert_bounded(cuts, left = 0, right = 0.5)
+  assert_vector(cuts)
+  assert_increasing(cuts)
+
+  # Extract information from simulation
+  df_sim <- data.frame(
+    # PLAF
+    PLAF = sim$data$PLAF,
+    PLAF_cut = cut(sim$data$PLAF, cuts, include.lowest = TRUE),
+
+    # Determine if a site is a variant, accounting for sequencing error.
+    variant =
+      ifelse(sim$data$WSAF < seq_error | sim$data$WSAF > (1 - seq_error), 0, 1),
+
+    # True variant
+    true_variant =
+      as.integer(!apply(sim$phased, 2, function(x) {all(x) || all(!x)}))
+  )
+
+  # Average over intervals of PLAF
+  df_sim_grouped <- dplyr::group_by(df_sim, .data$PLAF_cut) %>%
+    dplyr::summarise(m_variant = mean(.data$variant),
+                     m_true_variant = mean(.data$true_variant),
+                     bucket_size = dplyr::n())
+  df_sim_grouped <- as.data.frame(df_sim_grouped)
+
+  # Include midpoints
+  df_sim_grouped$midpoints <- cuts[-length(cuts)] + diff(cuts)/2
+
+  return(df_sim_grouped)
+}
