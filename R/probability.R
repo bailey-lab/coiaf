9ba3aef55c59845d13cd35385a959cdccca8181e
arisp99 <aris.paschalidis@gmail.com>
2020-06-09 11:19:13 -0400

    Added a couple of assert statements to the probability functions. In addition, cleaned up the functions for calculating theoretical coi curves by adding default values and adding some more documentation.

--- R/probability.R
+++ R/probability.R
@@ -26,6 +26,11 @@ rdirichlet <- function(shape = rep(1,3)) {
 #' @keywords internal
 
 rbetabinom <- function(n = 1, k = 10, alpha = 1, beta = 1) {
+  # Check inputs
+  assert_single_pos_int(n)
+  assert_pos_int(k)
+
+  # Draw from distribution
   p <- rbeta(n = n, shape1 = alpha, shape2 = beta)
   ret <- rbinom(n = n, size = k, prob = p)
   return(ret)
--- R/theoretical_coi.R
+++ R/theoretical_coi.R
@@ -4,19 +4,20 @@
 #' @description Generate theoretical COI curves. At each locus defined as:
 #' \eqn{1-p^coi-q^coi}. This expression is derived in the corresponding
 #' manuscript.
+#'
+#' @param coi_range The COIs for which the curve will be calculated.
+#' @param interval The interval over which the curve will be calculated.
 #' @param method The method we will use to calculate the theoretical COI.
 #' The method is either "1" or "2". The default value is "1".
-#'
-#' @param cois The COIs for which the curve will be calculated.
-#' @param interval The interval over which the curve will be calculated.
 #'
 #' @return The theoretical COI curves for the specified COIs and interval.
 #'
 #' @export
 
-theoretical_coi <- function(cois, interval, method = "1"){
+theoretical_coi <- function(coi_range, interval = seq(0, 0.5, l = 101),
+                            method = "1"){
   # Check inputs
-  assert_pos_int(cois, zero_allowed = FALSE)
+  assert_pos_int(coi_range, zero_allowed = FALSE)
   assert_vector(interval)
   assert_bounded(interval, left = 0, right = 0.5)
   assert_increasing(interval)
@@ -24,16 +25,16 @@ theoretical_coi <- function(cois, interval, method = "1"){
   assert_in(method, c("1", "2"))
 
   # Compute curve for the COIs
-  for (i in cois){
-    if (i == cois[1]){
+  for (i in coi_range){
+    if (i == coi_range[1]){
       curves <- data.frame(first = single_theoretical_coi(i, interval, method))
       colnames(curves) <- paste0("coi_", i)
     } else {
-      curves[[paste0("coi_", i)]] =
-        single_theoretical_coi(i, interval, method)
+      curves[[paste0("coi_", i)]] = single_theoretical_coi(i, interval, method)
     }
   }
 
+  # Include the PLAF in the final output and return
   curves$PLAF <- interval
   return(curves)
 }
@@ -45,16 +46,15 @@ theoretical_coi <- function(cois, interval, method = "1"){
 #' \eqn{1-p^coi-q^coi}. This expression is derived in the corresponding
 #' manuscript.
 #'
-#' @param coi The COI.
-#' @param interval The interval over which the curve will be calculated.
-#' @param method The method we will use to calculate the theoretical COI.
-#' The method is either "1" or "2". The default value is "1".
+#' @param coi The COI for which the curve will be calculated.
+#' @inheritParams theoretical_coi
 #'
 #' @return The theoretical COI curve for the specified interval.
 #'
 #' @keywords internal
 
-single_theoretical_coi <- function(coi, interval, method = "1"){
+single_theoretical_coi <- function(coi, interval = seq(0, 0.5, l = 101),
+                                   method = "1"){
   # Check inputs
   assert_single_pos_int(coi)
   assert_vector(interval)
