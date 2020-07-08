23a82e3dbae9bceeb9f2bd035a9009363943de1a
Aris Paschalidis <aris.paschalidis@gmail.com>
2020-07-08 18:31:32 -0400

    Updated the data stored and added a vignette to demonstrate how to run real data. Also some code to adjust the navigation bar in the website.

--- data-raw/example_real_data.R
+++ data-raw/example_real_data.R
@@ -2,5 +2,7 @@
 path = "~/Desktop/Malaria/COI data/"
 rmcl_wsafs   <- readRDS(paste0(path, "RMCL_wsafs_unique.rds"))
 example_real_data <- rmcl_wsafs[1:2]
+example_real_data$cat_region_1_vcf_0 <- example_real_data$cat_region_1_vcf_0[11:20, 1:1000]
+example_real_data$cat_region_1_vcf_1 <- example_real_data$cat_region_1_vcf_1[11:20, 1:1000]
 
 usethis::use_data(example_real_data, overwrite = TRUE)
