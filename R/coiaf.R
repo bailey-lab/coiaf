c9fcf55f3dfc8ef25c6414de8e014735c0876808
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-06-11 15:35:30 -0400

    Reworked the vignette demonstrating running a real sample. In addition, shrunk the dataset that comes with the package to make running real data more straightforward. Now the script describes running data when you are given the WSAFs for several samples as a matrix.

--- R/coiaf.R
+++ R/coiaf.R
@@ -13,10 +13,8 @@
 #' A small example dataset that contains within-sample allele frequencies
 #' (WSAFs) from a sample of individuals.
 #'
-#' @format A list of matrices. The list indicates the region of the genome and
-#' the VCF file where the samples came from. The two matrices have the same
-#' structure. The rows of the matrix indicate the patient and the columns of
-#' the matrix indicate the WSAF at each locus.
+#' @format A matrix of data. The rows of the matrix indicate the sample name and
+#'   the columns of the matrix indicate the WSAF at each locus.
 #'
 #' @source ftp://ngs.sanger.ac.uk/production/malaria/pfcommunityproject/Pf6/
 #'
@@ -27,5 +25,5 @@ NULL
 
 # Silence the R CMD check notes on the where function
 # This function comes from the tidyselect package but has not yet been exported
-# causing R to through notes at the user.
+# causing R to throw notes at the user.
 utils::globalVariables("where")
--- data-raw/example_real_data.R
+++ data-raw/example_real_data.R
@@ -1,8 +1,7 @@
 # Set the path and then access the data
 path = "~/Desktop/Malaria/COI data/"
 rmcl_wsafs   <- readRDS(paste0(path, "RMCL_wsafs_unique.rds"))
-example_real_data <- rmcl_wsafs[1:2]
-example_real_data$cat_region_1_vcf_0 <- example_real_data$cat_region_1_vcf_0[11:20, 1:1000]
-example_real_data$cat_region_1_vcf_1 <- example_real_data$cat_region_1_vcf_1[11:20, 1:1000]
+example_real_data <- rmcl_wsafs[[1]]
+example_real_data <- example_real_data[11:20, 1:1000]
 
 usethis::use_data(example_real_data, overwrite = TRUE)
