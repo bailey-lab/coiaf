86f3d4aae78b42f7ed4e11d7d67399da93972976
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-11-30 16:26:03 -0500

    Update names in analysis files

--- analysis/relatedness_example.R
+++ analysis/relatedness_example.R
@@ -9,36 +9,36 @@ coverage <- 100
 reps <- 20
 
 cois <- 2:10
-rels <- seq(0,0.9, 0.1)
+rels <- seq(0, 0.9, 0.1)
 pars <- expand.grid(k = cois, r = rels)
 results <- vector("list", nrow(pars))
 
-for(i in seq_len(nrow(pars))) {
-
+for (i in seq_len(nrow(pars))) {
   message(i)
   k <- pars$k[i]
   r <- pars$r[i]
 
   dat <- lapply(seq_len(reps), sim_biallelic, coi = k, plaf = p, coverage = coverage, relatedness = r)
 
-  disc_k <- lapply(dat, compute_coi, data_type = "sim", coi_method = "1")
+  disc_k <- lapply(dat, compute_coi, data_type = "sim", coi_method = "variant")
   disc_k <- unlist(lapply(disc_k, "[[", "coi"))
 
-  disc_k_2 <- lapply(dat, compute_coi, data_type = "sim", coi_method = "2")
+  disc_k_2 <- lapply(dat, compute_coi, data_type = "sim", coi_method = "frequency")
   disc_k_2 <- unlist(lapply(disc_k_2, "[[", "coi"))
 
-  cont_k <- lapply(dat, optimize_coi, data_type = "sim", coi_method = "1")
-  cont_k_2 <- lapply(dat, optimize_coi, data_type = "sim", coi_method = "2")
+  cont_k <- lapply(dat, optimize_coi, data_type = "sim", coi_method = "variant")
+  cont_k_2 <- lapply(dat, optimize_coi, data_type = "sim", coi_method = "frequency")
 
   r_est <- unlist(cont_k_2) - unlist(cont_k)
 
-  df <- data.frame("coi" = k, "r" = r,
-                   "rep" = seq_len(reps), "r_est" = r_est,
-                   "coi_d" = unlist(disc_k), "coi_d_2" = unlist(disc_k_2),
-                   "coi_c" = unlist(cont_k), "coi_c_2" = unlist(cont_k_2))
+  df <- data.frame(
+    "coi" = k, "r" = r,
+    "rep" = seq_len(reps), "r_est" = r_est,
+    "coi_d" = unlist(disc_k), "coi_d_2" = unlist(disc_k_2),
+    "coi_c" = unlist(cont_k), "coi_c_2" = unlist(cont_k_2)
+  )
 
   results[[i]] <- df
-
 }
 
 # plot
@@ -46,9 +46,12 @@ plot <- do.call(rbind, results) %>%
   dplyr::mutate(coi_lab = paste0("COI = ", coi))
 
 plot$coi_lab <- factor(plot$coi_lab,
-                       levels = c("COI = 2", "COI = 3", "COI = 4", "COI = 5",
-                                  "COI = 6", "COI = 7", "COI = 8", "COI = 9",
-                                  "COI = 10"))
+  levels = c(
+    "COI = 2", "COI = 3", "COI = 4", "COI = 5",
+    "COI = 6", "COI = 7", "COI = 8", "COI = 9",
+    "COI = 10"
+  )
+)
 
 ggplot(aes(x = r, y = r_est), data = plot) +
   geom_point(color = "blue", alpha = 0.7, show.legend = FALSE) +
@@ -57,5 +60,5 @@ ggplot(aes(x = r, y = r_est), data = plot) +
   theme_classic() +
   theme(axis.line = element_line()) +
   facet_wrap(~coi_lab) +
-  ylab("Method 2 Continous - Method 1 Continuous") + xlab("Relatedness")
-
+  ylab("Method 2 Continous - Method 1 Continuous") +
+  xlab("Relatedness")
