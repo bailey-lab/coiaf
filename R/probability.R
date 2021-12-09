2121f12f11b58444172423f980c68d3d37bc9be6
Aris Paschalidis <aris.paschalidis@gmail.com>
2021-12-09 16:50:08 -0500

    Improve documentation

--- R/probability.R
+++ R/probability.R
@@ -1,28 +1,43 @@
 #------------------------------------------------
-#' @title Draw from Dirichlet distribution
+#' Dirichlet distribution
 #'
-#' @description Draw from Dirichlet distribution given a vector of shape
-#'   parameters.
+#' Draw from a Dirichlet distribution given a vector of shape parameters. The
+#' number of observations is inferred from the length of the shape vector.
 #'
 #' @param shape Vector of shape parameters.
 #'
+#' @seealso [rgamma()] for additional details.
+#' @family distributions
 #' @keywords internal
+#' @examples
+#' rdirichlet(c(1, 1, 1))
+#' rdirichlet(c(1, 3, 2))
 rdirichlet <- function(shape = rep(1, 3)) {
+  # Check input
+  assert_pos(shape)
+  assert_vector(shape)
+
+  # Draw from distribution
   x <- rgamma(length(shape), shape = shape, rate = max(shape))
   return(x / sum(x))
 }
 
 #------------------------------------------------
-#' @title Draw from Beta-binomial distribution
+#' Beta-binomial distribution
 #'
-#' @description Draw from Beta-binomial distribution.
+#' Draw from a Beta-binomial distribution.
 #'
 #' @param n Number of draws.
 #' @param k Number of binomial trials.
 #' @param alpha First shape parameter of beta distribution.
 #' @param beta Second shape parameter of beta distribution.
 #'
+#' @seealso [rbeta()] and [rbinom()] for additional details.
+#' @family distributions
 #' @keywords internal
+#' @examples
+#' rbetabinom()
+#' rbetabinom(n = 10, k = 10)
 rbetabinom <- function(n = 1, k = 10, alpha = 1, beta = 1) {
   # Check inputs
   assert_single_pos_int(n)
