5defdef598b59251f6dcf91c25bf78a70a15d222
Aris Paschalidis <aris.paschalidis@gmail.com>
2022-03-29 18:24:03 -0400

    Define method for simulated data

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -75,3 +75,62 @@ bootstrap_ci.default <- function(data,
     }
   )
 }
+
+#' @rdname bootstrap_ci
+#' @export
+bootstrap_ci.sim <- function(data,
+                             max_coi = 25,
+                             seq_error = 0.01,
+                             coi_method = c("variant", "frequency"),
+                             solution_method = c("discrete", "continuous"),
+                             use_bins = FALSE,
+                             bin_size = 20,
+                             replicates = 100,
+                             parallel = TRUE,
+                             ncpus = 8) {
+  # Argument matching
+  coi_method <- rlang::arg_match(coi_method)
+  solution_method <- rlang::arg_match(solution_method)
+
+  # Define statistic function
+  bootstrap_statistic <- function(data,
+                                  indices,
+                                  sim_data,
+                                  solution_method,
+                                  ...) {
+    data <- sim_data
+    data$data <- data$data[indices, ]
+
+    # Determine function call based on solution_method
+    switch(solution_method,
+      discrete = compute_coi(data, "sim", ...)$coi,
+      continuous = optimize_coi(data, "sim", ...)
+    )
+  }
+
+  # Bootstrap
+  boot_out <- boot::boot(
+    data = seq_len(nrow(data$data)),
+    statistic = bootstrap_statistic,
+    R = replicates,
+    parallel = if (parallel) "multicore" else "no",
+    ncpus = ncpus,
+    sim_data = data,
+    solution_method = solution_method,
+    max_coi = 25,
+    seq_error = 0.01,
+    coi_method = coi_method,
+    use_bins = FALSE,
+    bin_size = 20
+  )
+
+  tryCatch(
+    broom::tidy(boot_out, conf.int = TRUE) %>%
+      dplyr::rename(coi = .data$statistic),
+    error = function(e) {
+      broom::tidy(boot_out, conf.int = FALSE) %>%
+        tibble::add_column(conf.low = NaN, conf.high = NaN) %>%
+        dplyr::rename(coi = .data$statistic)
+    }
+  )
+}
